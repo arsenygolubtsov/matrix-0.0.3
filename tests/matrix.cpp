5db3548813656e3ed849fd5740ede4ec037c4d03
arsenygolubtsov <32494075+arsenygolubtsov@users.noreply.github.com>
2018-03-08 01:11:56 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -114,3 +114,88 @@ TEST_CASE("mul matrix")
     REQUIRE( input3 == ostream.str() );
 }
 
+TEST_CASE("selfadd matrix")
+{
+    std::string input1{
+        "3, 3\n"
+        "1 2 3\n"
+        "4 5 6\n"
+        "7 8 9" }; 
+    std::string input2{
+        "3, 3\n"
+        "9 8 7\n"
+        "6 5 4\n"
+        "3 2 1" };
+    std::string input3{
+        "3, 3\n"
+        "10 10 10\n"
+        "10 10 10\n"
+        "10 10 10" };
+    matrix_t matrix1, matrix2, matrix3;   
+    std::istringstream istream1{ input1 };
+    std::istringstream istream2{ input2 };
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    matrix1 += matrix2;
+    std::ostringstream ostream;
+    matrix1.write( ostream );
+    REQUIRE( input3 == ostream.str() );
+}
+
+TEST_CASE("selfsub matrix")
+{
+    std::string input1{
+        "3, 3\n"
+        "1 2 3\n"
+        "4 5 6\n"
+        "7 8 9" }; 
+    std::string input2{
+        "3, 3\n"
+        "0 1 2\n"
+        "3 4 5\n"
+        "6 7 8" };
+    std::string input3{
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+    matrix_t matrix1, matrix2, matrix3;   
+    std::istringstream istream1{ input1 };
+    std::istringstream istream2{ input2 };
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    matrix1 -= matrix2;
+    std::ostringstream ostream;
+    matrix1.write( ostream );
+    REQUIRE( input3 == ostream.str() );
+}
+
+TEST_CASE("selfmul matrix")
+{
+    std::string input1{
+        "3, 3\n"
+        "1 2 3\n"
+        "1 2 3\n"
+        "1 2 3" }; 
+    std::string input2{
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    std::string input3{
+        "3, 3\n"
+        "14 14 14\n"
+        "14 14 14\n"
+        "14 14 14" };
+    matrix_t matrix1, matrix2, matrix3;   
+    std::istringstream istream1{ input1 };
+    std::istringstream istream2{ input2 };
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    matrix1 *= matrix2;
+    std::ostringstream ostream;
+    matrix1.write( ostream );
+    REQUIRE( input3 == ostream.str() );
+}
+
+
